cb5619bbd93e48b57ea053a5d30d5ecccd7a3bf3
vildan <108195261+vildan9@users.noreply.github.com>
2022-07-26 19:52:43 +0300

    Update clock.js
    açıklamalar temizlendi

--- clock.js
+++ clock.js
@@ -9,15 +9,15 @@ function time ()
     let hours = moment.getHours(); 
    
     let minutes = moment.getMinutes(); 
-    // saniye ekledik
+  
     let seconds = moment.getSeconds(); 
-    // güne ekstra zaman atamamızın sebebi aşağıdaki verdiğimiz değerler ile sorunsuz çalışa bilsin diye aksi takdirde çalışmaz
+
     var d = new Date(); 
-    // günleri ekledik
+ 
     var days= ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];  
-    // id tanımmladık
+    
     let iceaktarma= document.querySelector("#myClock") 
-    // burda oluşturduğumuz değerleri ekrana yazdırıyoruz
+   
     iceaktarma.innerHTML= hours + ":" + minutes +":" + seconds + " " + days[d.getDay()]; 
 }
 let now = setInterval(time,100); 
